e56c3e674bc5acf7a72edd6c6b528982895f2876
Tamas Varga <tamas.varga@flex.com>
2018-01-19 05:41:34 -0800

    [Studio] Add screen "issuedetails"

---
+++ client/controllers/app/issuedetails.js
@@ -0,0 +1,5 @@
+angular.module('app').controller('app_issuedetails', app_issuedetails);
+function app_issuedetails($scope, app) {
+    'use strict';
+    app.init($scope);
+}
---
+++ client/mocks/issuedetails.js
@@ -0,0 +1,3 @@
+'use strict';
+window.mocks = window.mocks || {};
+window.mocks.issuedetails = window.mocks.issuedetails || {};
