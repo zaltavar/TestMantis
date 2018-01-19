41a27f1bc38020d970d17636edafc6d97c454629
Tamas Varga <tamas.varga@flex.com>
2018-01-19 05:00:17 -0800

    [Studio] Add screen "issuedetails"

--- client/states.js
+++ client/states.js
@@ -60,5 +60,14 @@ angular.module('app').config(function ($stateProvider) {
                 }
             }
         }
+    }).state('app.issuedetails', {
+        views: {
+            app: {
+                controller: 'app_issuedetails',
+                templateProvider: function (app) {
+                    return app.templateProvider('app.issuedetails');
+                }
+            }
+        }
     });
 });
